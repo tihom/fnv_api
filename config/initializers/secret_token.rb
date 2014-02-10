694c1ba07316b372219a570a6697a8c5d8195cdc
Mohit Aggarwal <mohitagg@gmail.com>
2014-02-10 02:35:15 -0800

    initalized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FnvApi::Application.config.secret_key_base = '45935cf4ae4faf4afac7fc54b8a4d7d8c6a4fa0f73d50d481a63fa90069d180b505b0b089890bf6c385f28b44243326eb07f21b233dc5537097e1b55a49c7d17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FnvApi::Application.config.session_store :cookie_store, key: '_fnv_api_session'
